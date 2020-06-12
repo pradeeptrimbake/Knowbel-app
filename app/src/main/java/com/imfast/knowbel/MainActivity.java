49858e5526fac4bcefa29fc24256bde00fce167d
AnimeManiac <45123403+AnimeManiac@users.noreply.github.com>
2020-06-12 14:02:03 +0530

    Added source code files.
    Basic webview layout loading webpage -https://knowbel.imfast.io/
    
    Webpage page hosts flipbook of our magazine

---
+++ app/src/main/java/com/imfast/knowbel/MainActivity.java
@@ -0,0 +1,35 @@
+package com.imfast.knowbel;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webView.getSettings().setBuiltInZoomControls(true);
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://knowbel.imfast.io/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
